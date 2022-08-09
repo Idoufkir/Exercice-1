38d516da392052c8ebb8fd3981f4e1febd7de4db
Mustafa IDOUFKIR <mustafa.idoufkir@gmail.com>
2022-08-09 19:23:56 +0100

    Update exercice

--- controllers/product.js
+++ controllers/product.js
@@ -3,6 +3,8 @@ const Product = require('../model/product.model');
 
 let message = "";
 
+
+// GET	/product/	List des produits
 exports.getProducts = async (req, res) => {
     Product.find()
         .then(data => {
@@ -21,37 +23,32 @@ exports.getProducts = async (req, res) => {
         });
 };
 
+
+// POST / product / Ajout de produit
 exports.postProduct = (req, res) => {
     const addProduct = new Product(req.body);
 
-    addProduct.save((err, product) => {
-        if (err) {
-            return res.status(500).send({
+    addProduct.save((err, data) => {
+        if (err || data === undefined) {
+            return res.status(400).send({
                 success: false,
-                message: err.message || "Some error occurred while creating the product."
+                message: err.message || "Some error occurred while creating the Product."
             });
         }
-        res.json(product);
+        res.send({
+            success: true,
+            message: "Product added successfully!",
+            data: data
+        });
     });
-};
+}
 
-// exports.getProduct = async (req, res, next) => {
-//     let product
-//     try {
-//         product = await Product.findById(req.params.id)
-//         if (product == null) {
-//             return res.status(404).json({ message: 'Cannot find Product!' })
-//         }
-//     } catch (err) {
-//         return res.status(500).json({ message: err.message })
-//     }
-//     res.product = product
-//     next()
-// }
 
+
+// DELETE / product / {: product_id }	Suppression de produit
 exports.deleteProduct = async (req, res) => {
-    Product.findByIdAndDelete(req.params.product_id, function (err, product) {
-        if (err || product == null) {
+    Product.findByIdAndDelete(req.params.product_id, function (err, data) {
+        if (err || data == null) {
             return res.status(404).send({
                 success: false,
                 message: "Cannot find Product!"
@@ -60,6 +57,86 @@ exports.deleteProduct = async (req, res) => {
         res.send({
             success: true,
             message: "Product successfully deleted!",
+            data: data
+        });
+    });
+};
+
+
+// PATCH / product / {: product_id }	Mise à jour de produit
+exports.updateProduct = async (req, res) => {
+    Product.findByIdAndUpdate(req.params.product_id, req.body, { new: true }, function (err, data) {
+        if (err || data == null) {
+            return res.status(404).send({
+                success: false,
+                message: "Cannot find Product!"
+            });
+        }
+        res.send({
+            success: true,
+            message: "Product successfully updated!",
+            data: data
+        });
+    });
+};
+
+
+// GET / product / {: product_id } / variants / List des variantes d'un produit {product_id}
+exports.getVariantsProduct = async (req, res) => {
+    Product.findById(req.params.product_id, function (err, product) {
+        if (err || product == null) {
+            return res.status(404).send({
+                success: false,
+                message: "Cannot find Product!"
+            });
+        }
+        res.send({
+            success: true,
+            message: "variants successfully retrieved!",
+            data: product.variants
+        });
+    });
+};
+
+
+// GET / product / {: product_id } / variants / {: variant_id }	List de la variante { variant_id } du produit { product_id }
+exports.getVariantById = async (req, res) => {
+    Product.findById(req.params.product_id, function (err, product) {
+        if (err || product == null) {
+            return res.status(404).send({
+                success: false,
+                message: "Cannot find Product!"
+            });
+        }
+        const variant = product.variants.find(variant => variant._id == req.params.variant_id)
+        if (variant == null) {
+            return res.status(404).send({
+                success: false,
+                message: "Cannot find Variant!"
+            });
+        }
+        res.send({
+            success: true,
+            message: "Variant successfully retrieved!",
+            data: variant
+        });
+    });
+};
+
+
+
+// GET / product / {: product_id }	List du produit { product_id }
+exports.getProductById = async (req, res) => {
+    Product.findById(req.params.product_id, function (err, product) {
+        if (err || product == null) {
+            return res.status(404).send({
+                success: false,
+                message: "Cannot find Product!"
+            });
+        }
+        res.send({
+            success: true,
+            message: "Product successfully retrieved!",
             data: product
         });
     });
--- routes/product.js
+++ routes/product.js
@@ -4,20 +4,46 @@ const products = require('../controllers/product');
 const authHeader = require('../middleware/middleware');
 const api_key = process.env.API_KEY;
 
+
+// GET / product / List des produits
 router.get('/', (req, res) => {
     products.getProducts(req, res);
 });
 
+
+// POST / product / Ajout de produit
 router.post('/', authHeader(api_key), (req, res) => {
     products.postProduct(req, res);
 });
 
+
+// DELETE / product / {: product_id }	Suppression de produit
 router.delete('/:product_id', authHeader(api_key), (req, res) => {
     products.deleteProduct(req, res);
 });
 
-// router.post('/', authHeader(api_key), (req, res) => {
-//     products.getProducts(req, res);
-// });
+
+// PATCH / product / {: product_id }	Mise à jour de produit
+router.patch('/:product_id/', authHeader(api_key), (req, res) => {
+    products.updateProduct(req, res);
+});
+
+
+// GET / product / {: product_id } / variants / List des variantes d'un produit {product_id}
+router.get('/:product_id/variants', (req, res) => {
+    products.getVariantsProduct(req, res);
+});
+
+
+// GET / product / {: product_id } / variants / {: variant_id }	List de la variante { variant_id } du produit { product_id }
+router.get('/:product_id/variants/:variant_id', (req, res) => {
+    products.getVariantById(req, res);
+});
+
+// GET / product / {: product_id }	List du produit { product_id }
+router.get('/:product_id', (req, res) => {
+    products.getProductById(req, res);
+});
+
 
 module.exports = router;
